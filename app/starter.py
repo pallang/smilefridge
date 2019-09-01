ace47baf63a6aa26e655f06e3b3e5f69244b0a06
pallang <53940748+pallang@users.noreply.github.com>
2019-09-01 22:12:17 +0900

    korean name

--- app/starter.py
+++ app/starter.py
@@ -130,7 +130,7 @@ class LoginForm(Form):
 class RegistrationForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     username = StringField('Username', validators=[Required(), Length(1, 64),
-                                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, '
+                                                   Regexp('^[가-힣A-Za-z][가-힣A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, '
                                                           'numbers, dots or underscores')])
     password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm Password', validators=[Required()])
